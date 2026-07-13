99a502e19e97885169a35b6c43d5c07ead4f1c0f
Philipp <philipp.altmann@ifi.lmu.de>
2026-07-13 19:11:15 +0200

    Integrated new baselines and ablations.

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,8 +1,10 @@
 import src.controllers.controller as controller
 import src.controllers.actor_critic as actor_critic
 import src.controllers.mate as mate
+import src.controllers.mediate as mediate
 import src.controllers.drive as drive
 import src.controllers.lio as lio
+import src.controllers.ltos as ltos
 import src.controllers.inequity_aversion as inequity_aversion
 
 def make(params):
@@ -13,12 +15,22 @@ def make(params):
         return actor_critic.ActorCritic(params)
     if algorithm_name.startswith("LIO"):
         return lio.LIO(params)
+    if algorithm_name.startswith("LToS"):
+        return ltos.LToS(params)
     if algorithm_name.startswith("IA"):
         return inequity_aversion.InequityAversion(params)
-    if algorithm_name.startswith("MATE-TD"):
+    if algorithm_name.startswith("MATE"):
         params["mate_mode"] = "td_error"
         return mate.MATE(params)
-    if algorithm_name.startswith("DRIVE-TD"):
+    if algorithm_name == "MEDIATE": # Synchronous by default
         params["mate_mode"] = "td_error"
+        params["consensus_on"] = True
+        return mediate.MEDIATE(params)    
+    if algorithm_name.startswith("DRIVE"):
+        params["mate_mode"] = "td_error"
+        if "-default" in algorithm_name: params["reciprocal_trust"] = False; params["response_mode"] = "mean"
+        if "-instant" in algorithm_name: params["response_mode"] = "instant"
+        if "-retrust" in algorithm_name: params["reciprocal_trust"] = True
+        if "-ungated" in algorithm_name: params["mate_mode"] = "ungated"
         return drive.DRIVE(params)
     raise ValueError("Unknown algorithm '{}'".format(algorithm_name))
