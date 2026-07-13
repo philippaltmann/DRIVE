7cc0f3e23c71ef215d5b1e1ee66a02cb89e6c788
Philipp <philipp.altmann@ifi.lmu.de>
2026-07-13 23:14:59 +0200

    Added per-agent, per-step noise pertubation

--- src/domains.py
+++ src/domains.py
@@ -2,46 +2,46 @@ import src.environments.matrix_games as matrix_games
 import src.environments.coin_game as coin_game
 import src.environments.harvest as harvest
 import numpy
+ 
+def drift_function(function_name, params, modes=4, constant=10): # constant=constant, fraction=fraction
+    """
+    modes: int(default 4): Number of changes 
+    constant: int (default: 10): Constant shift
+    """
+    n_epochs = params["nr_epochs"]
+    eta = fraction = modes / n_epochs
+    duration = n_epochs / modes 
 
-def drift_function(function_name, constant=10, fraction=0.001):
-    if function_name == "identity":
-        return lambda t,x: x
-    if function_name == "shift_pos":
-        return lambda t,x: x + constant
-    if function_name == "shift_neg":
-        return lambda t,x: x - constant
+    if function_name == "identity": return lambda m,u: u
+    if function_name == "shift_pos": return lambda m,u: u + constant
+    if function_name == "shift_neg": return lambda m,u: u - constant
     if function_name == "stepwise_increase":
-        return lambda t,x: (numpy.floor(t/1000) + constant)*x
-    if function_name == "scale_up":
-        return lambda t,x: constant*x
-    if function_name == "scale_down":
-        return lambda t,x: (1.0/constant)*x
-    if function_name == "linear":
-        return lambda t,x: (1+t*fraction)*x
-    if function_name == "quotient":
-        return lambda t,x: (1.0/(t+1))*x
+        return lambda m,u: u * (numpy.floor(eta * m) + constant)
+        return lambda m,u: u * ((numpy.floor(eta * m) + 1) * constant)
+    if function_name == "scale_up": return lambda m,u: u * constant
+    if function_name == "scale_down": return lambda m,u: u * (1.0 / constant)
+    if function_name == "linear": return lambda m,u: u * (eta * m + 1)
     if function_name == "exponential_increase":
-        return lambda t,x: ((1+fraction)**t)*x
+        return lambda m,u: u * numpy.exp(eta * m)
     if function_name == "exponential_decay":
-        return lambda t,x: ((1+fraction)**(-t))*x
-    if function_name == "cos_widened":
-        double_frac = 2*fraction
-        norm = 5000
-        return lambda t,x: (fraction + t*numpy.cos(double_frac*t)*numpy.cos(double_frac*t)/norm)*x
+        return lambda m,u: u * numpy.exp(-eta * m)
+    if function_name == "cos_widened":        
+        return lambda m,u: u * (m / n_epochs) * numpy.cos(2 * eta * m)**2
     if function_name == "cos_damped":
-        double_frac = 2*fraction
-        norm = 5000
-        return lambda t,x: (fraction + (norm-t)*numpy.cos(double_frac*t)*numpy.cos(double_frac*t)/norm)*x
+        return lambda m,u: u * (1 - m / n_epochs) * numpy.cos(2 * eta * m)**2
+    if function_name == "noisy": # Per-agent per-step pertubtion
+        sigma = params["d"]; print("sigma: ", sigma)       
+        return lambda t,x: x + numpy.random.normal(0, sigma, size=(params['nr_agents'],))
 
 def make(params):
     domain_name = params["domain_name"]
     if domain_name.startswith("Matrix-"):
-        params["R_max"] = 3
+        params["R_max"] = 3; params["d"] = 1
         return matrix_games.make(params)
     if domain_name.startswith("CoinGame-"):
-        params["R_max"] = 2
+        params["R_max"] = 2; params["d"] = 1 / int(domain_name[-1]) # 0.5
         return coin_game.make(params)
     if domain_name.startswith("Harvest-"):
-        params["R_max"] = 0.25
+        params["R_max"] = 0.25; params["d"] = 0.25
         return harvest.make(params)
     raise ValueError("Unknown domain '{}'".format(domain_name))
