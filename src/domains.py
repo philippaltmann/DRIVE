89a1888883a4d65701d32ea274b5e26230821b3c
Philipp <philipp.altmann@ifi.lmu.de>
2026-07-17 16:07:14 +0200

    Simplified domain initialization

--- src/domains.py
+++ src/domains.py
@@ -3,37 +3,35 @@ import src.environments.coin_game as coin_game
 import src.environments.harvest as harvest
 import numpy
  
-def drift_function(function_name, params, modes=4, constant=10): # constant=constant, fraction=fraction
-    """
-    modes: int(default 4): Number of changes 
-    constant: int (default: 10): Constant shift
-    """
+def drift_function(params, modes=4, constant=10):
+    """ modes: int(default 4): Number of changes 
+    constant: int (default: 10): Constant shift """
+    drift = params["drift"] if "drift" in params else "identity"
     n_epochs = params["nr_epochs"]
     eta = fraction = modes / n_epochs
     duration = n_epochs / modes 
 
-    if function_name == "identity": return lambda m,u: u
-    if function_name == "shift_pos": return lambda m,u: u + constant
-    if function_name == "shift_neg": return lambda m,u: u - constant
-    if function_name == "stepwise_increase":
+    if drift == "identity": return lambda m,u: u
+    if drift == "shift_pos": return lambda m,u: u + constant
+    if drift == "shift_neg": return lambda m,u: u - constant
+    if drift == "stepwise_increase":
         return lambda m,u: u * (numpy.floor(eta * m) + constant)
-        return lambda m,u: u * ((numpy.floor(eta * m) + 1) * constant)
-    if function_name == "scale_up": return lambda m,u: u * constant
-    if function_name == "scale_down": return lambda m,u: u * (1.0 / constant)
-    if function_name == "linear": return lambda m,u: u * (eta * m + 1)
-    if function_name == "exponential_increase":
+    if drift == "scale_up": return lambda m,u: u * constant
+    if drift == "scale_down": return lambda m,u: u * (1.0 / constant)
+    if drift == "linear": return lambda m,u: u * (eta * m + 1)
+    if drift == "exponential_increase":
         return lambda m,u: u * numpy.exp(eta * m)
-    if function_name == "exponential_decay":
+    if drift == "exponential_decay":
         return lambda m,u: u * numpy.exp(-eta * m)
-    if function_name == "cos_widened":        
+    if drift == "cos_widened":        
         return lambda m,u: u * (m / n_epochs) * numpy.cos(2 * eta * m)**2
-    if function_name == "cos_damped":
+    if drift == "cos_damped":
         return lambda m,u: u * (1 - m / n_epochs) * numpy.cos(2 * eta * m)**2
-    if function_name == "noisy": # Per-agent per-step pertubtion
+    if drift == "noisy": # Per-agent per-step pertubtion
         sigma = params["d"]; print("sigma: ", sigma)       
         return lambda t,x: x + numpy.random.normal(0, sigma, size=(params['nr_agents'],))
 
-def make(params):
+def make_env(params):
     domain_name = params["domain_name"]
     if domain_name.startswith("Matrix-"):
         params["R_max"] = 3; params["d"] = 1
@@ -42,6 +40,12 @@ def make(params):
         params["R_max"] = 2; params["d"] = 1 / int(domain_name[-1]) # 0.5
         return coin_game.make(params)
     if domain_name.startswith("Harvest-"):
-        params["R_max"] = 0.25; params["d"] = 0.25
+        params["R_max"] = 0.25; params["d"] = 0.25; params['reciprocal_trust'] = True
         return harvest.make(params)
     raise ValueError("Unknown domain '{}'".format(domain_name))
+
+def make(params):
+    env = make_env(params); env.reset()
+    params["drift_function"] = drift_function(params)
+    return env, params
+    
