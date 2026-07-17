f11f0b81c15218c5b8630c094f6f37553e4e1cd6
Philipp <philipp.altmann@ifi.lmu.de>
2026-07-17 16:24:33 +0200

    Updated run script

--- run.sh
+++ run.sh
@@ -1,113 +1,49 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# 1. Performance Evaluation
+ENVS=(
+  "Matrix-IPD"
+  "CoinGame-2"
+  "CoinGame-4"
+  "Harvest-12"
+)
 
-python train.py Matrix-IPD DRIVE-TD identity
-python train.py Matrix-IPD MATE-TD identity
-python train.py Matrix-IPD LIO identity
-python train.py Matrix-IPD IAC identity
+DRIFTS=(
+  "identity"
+  "linear"
+  "exponential_decay"
+  "stepwise_increase"
+  "cos_damped"
+  "noisy"
+)
 
-python train.py CoinGame-2 DRIVE-TD identity
-python train.py CoinGame-2 MATE-TD identity
-python train.py CoinGame-2 LIO identity
-python train.py CoinGame-2 IAC identity
+ALGS=(
+  "DRIVE"
+  "MATE"
+  "MEDIATE"
+  "LToS"
+  "LIO"
+  "IAC"
+  "IA"
+)
 
-python train.py CoinGame-4 DRIVE-TD identity
-python train.py CoinGame-4 MATE-TD identity
-python train.py CoinGame-4 LIO identity
-python train.py CoinGame-4 IAC identity
+# Optional seed argument
+SEED="${1:-}"
 
-python train.py Harvest-12 DRIVE-TD identity
-python train.py Harvest-12 MATE-TD identity
-python train.py Harvest-12 LIO identity
-python train.py Harvest-12 IAC identity
+# Timestamp for unique logfiles
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 
-# 2. Robustness against Reward Drift
+for ENV in "${ENVS[@]}"; do
+  for ALG in "${ALGS[@]}"; do
+    for DRIFT in "${DRIFTS[@]}"; do
 
-# Linear Drift
+      if [[ -n "$SEED" ]]; then
+        python -m train "$ALG" "$ENV" -d "$DRIFT" -s "$SEED" \
+          > "./_logfiles/${ENV}_${DRIFT}_${ALG}_${SEED}.out" &
+      else
+        python -m train "$ALG" "$ENV" -d "$DRIFT" \
+          > "./_logfiles/${ENV}_${DRIFT}_${ALG}_${TIMESTAMP}.out" &
+      fi
 
-python train.py Matrix-IPD DRIVE-TD linear
-python train.py Matrix-IPD MATE-TD linear
-python train.py Matrix-IPD LIO linear
-python train.py Matrix-IPD IAC linear
-
-python train.py CoinGame-2 DRIVE-TD linear
-python train.py CoinGame-2 MATE-TD linear
-python train.py CoinGame-2 LIO linear
-python train.py CoinGame-2 IAC linear
-
-python train.py CoinGame-4 DRIVE-TD linear
-python train.py CoinGame-4 MATE-TD linear
-python train.py CoinGame-4 LIO linear
-python train.py CoinGame-4 IAC linear
-
-python train.py Harvest-12 DRIVE-TD linear
-python train.py Harvest-12 MATE-TD linear
-python train.py Harvest-12 LIO linear
-python train.py Harvest-12 IAC linear
-
-# Exponential Decay
-
-python train.py Matrix-IPD DRIVE-TD exponential_decay
-python train.py Matrix-IPD MATE-TD exponential_decay
-python train.py Matrix-IPD LIO exponential_decay
-python train.py Matrix-IPD IAC exponential_decay
-
-python train.py CoinGame-2 DRIVE-TD exponential_decay
-python train.py CoinGame-2 MATE-TD exponential_decay
-python train.py CoinGame-2 LIO exponential_decay
-python train.py CoinGame-2 IAC exponential_decay
-
-python train.py CoinGame-4 DRIVE-TD exponential_decay
-python train.py CoinGame-4 MATE-TD exponential_decay
-python train.py CoinGame-4 LIO exponential_decay
-python train.py CoinGame-4 IAC exponential_decay
-
-python train.py Harvest-12 DRIVE-TD exponential_decay
-python train.py Harvest-12 MATE-TD exponential_decay
-python train.py Harvest-12 LIO exponential_decay
-python train.py Harvest-12 IAC exponential_decay
-
-# Stepwise Increase
-
-python train.py Matrix-IPD DRIVE-TD stepwise_increase
-python train.py Matrix-IPD MATE-TD stepwise_increase
-python train.py Matrix-IPD LIO stepwise_increase
-python train.py Matrix-IPD IAC stepwise_increase
-
-python train.py CoinGame-2 DRIVE-TD stepwise_increase
-python train.py CoinGame-2 MATE-TD stepwise_increase
-python train.py CoinGame-2 LIO stepwise_increase
-python train.py CoinGame-2 IAC stepwise_increase
-
-python train.py CoinGame-4 DRIVE-TD stepwise_increase
-python train.py CoinGame-4 MATE-TD stepwise_increase
-python train.py CoinGame-4 LIO stepwise_increase
-python train.py CoinGame-4 IAC stepwise_increase
-
-python train.py Harvest-12 DRIVE-TD stepwise_increase
-python train.py Harvest-12 MATE-TD stepwise_increase
-python train.py Harvest-12 LIO stepwise_increase
-python train.py Harvest-12 IAC stepwise_increase
-
-# Damped Cosine
-
-python train.py Matrix-IPD DRIVE-TD cos_damped
-python train.py Matrix-IPD MATE-TD cos_damped
-python train.py Matrix-IPD LIO cos_damped
-python train.py Matrix-IPD IAC cos_damped
-
-python train.py CoinGame-2 DRIVE-TD cos_damped
-python train.py CoinGame-2 MATE-TD cos_damped
-python train.py CoinGame-2 LIO cos_damped
-python train.py CoinGame-2 IAC cos_damped
-
-python train.py CoinGame-4 DRIVE-TD cos_damped
-python train.py CoinGame-4 MATE-TD cos_damped
-python train.py CoinGame-4 LIO cos_damped
-python train.py CoinGame-4 IAC cos_damped
-
-python train.py Harvest-12 DRIVE-TD cos_damped
-python train.py Harvest-12 MATE-TD cos_damped
-python train.py Harvest-12 LIO cos_damped
-python train.py Harvest-12 IAC cos_damped
+    done
+  done
+done
