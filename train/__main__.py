17ad2b5a5ca339ece87476303a909fcc794e6eff
Philipp <philipp.altmann@ifi.lmu.de>
2026-07-17 16:25:48 +0200

    Moved settings to training

--- train/__main__.py
+++ train/__main__.py
@@ -4,7 +4,7 @@ import random
 import numpy as np
 import torch as th
 
-from settings import params
+from train.settings import params
 import src.domains as domains
 import src.algorithms as algorithms
 import src.experiments as experiments
